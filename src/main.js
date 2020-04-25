d1a6fab989c72edf07dbbc6637a8614a6b9751e5
Shashank Shekhar <shashank7200@gmail.com>
2020-04-25 17:24:21 +0530

    updated the code for main.js to create installationn script for vue comp

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import VueLinkPreview from './VueLinkPreview.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+function install(Vue) {
+	if (install.installed) return
+	install.installed = true
+	Vue.component('VueLinkPreview', VueLinkPreview)
+}
+
+const plugin = {
+	install
+}
+
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.vue
+}
+
+if (GlobalVue) {
+	GlobalVue.use(plugin)
+}
+
+VueLinkPreview.install = install
+
+export default VueLinkPreview
