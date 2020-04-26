12c5af3549b9771060f22c5dc25529b429b5ccd8
Shashank Shekhar <shashank7200@gmail.com>
2020-04-26 17:20:23 +0530

    refactored and added eslint support

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,6 +2,7 @@
 
 import vue from "rollup-plugin-vue";
 import buble from '@rollup/plugin-buble';
+import { eslint } from 'rollup-plugin-eslint';
 import commonjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
 import resolve from '@rollup/plugin-node-resolve';
@@ -13,32 +14,33 @@ import cleanup from 'rollup-plugin-cleanup';
 const argv = minimist(process.argv.slice(2));
 
 const config = {
-  input: "src/main.js",
-  output: {
-    name: "VueLinkPreview",
-    exports: "named"
-  },
-  exclude: 'node_modules/**',
-  plugins: [
-	vue({
-		css: true,
-		compileTemplate: true,
-		template: {
-		  isProduction: true
-		}
-	  }),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production")
-	}),
-	resolve(),
-	peerDepsExternal(),
-    commonjs(),
-	buble(),
-	cleanup({
-		comments: true,
-	}),
-	terser()
-  ]
+	input: "src/main.js",
+	output: {
+		name: "VueLinkPreview",
+		exports: "named"
+	},
+	exclude: 'node_modules/**',
+	plugins: [
+		vue({
+			css: true,
+			compileTemplate: true,
+			template: {
+				isProduction: true
+			}
+		}),
+		eslint(),
+		replace({
+			"process.env.NODE_ENV": JSON.stringify("production")
+		}),
+		resolve(),
+		peerDepsExternal(),
+		commonjs(),
+		buble(),
+		cleanup({
+			comments: false,
+		}),
+		terser()
+	]
 };
 
 // // Only minify browser (iife) version
