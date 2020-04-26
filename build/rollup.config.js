fc2ef374a96386b3a5500d1825b8d6090f37e497
Shashank Shekhar <shashank7200@gmail.com>
2020-04-26 17:14:54 +0530

    updated rollup config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,11 +1,14 @@
 // rollup.config.js
 
 import vue from "rollup-plugin-vue";
-import buble from "rollup-plugin-buble";
+import buble from '@rollup/plugin-buble';
 import commonjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
-import uglify from "rollup-plugin-uglify-es";
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from "rollup-plugin-terser";
 import minimist from "minimist";
+import cleanup from 'rollup-plugin-cleanup';
 
 const argv = minimist(process.argv.slice(2));
 
@@ -15,26 +18,33 @@ const config = {
     name: "VueLinkPreview",
     exports: "named"
   },
+  exclude: 'node_modules/**',
   plugins: [
+	vue({
+		css: true,
+		compileTemplate: true,
+		template: {
+		  isProduction: true
+		}
+	  }),
     replace({
       "process.env.NODE_ENV": JSON.stringify("production")
-    }),
+	}),
+	resolve(),
+	peerDepsExternal(),
     commonjs(),
-    vue({
-      css: true,
-      compileTemplate: true,
-      template: {
-        isProduction: true
-      }
-    }),
-    buble()
+	buble(),
+	cleanup({
+		comments: true,
+	}),
+	terser()
   ]
 };
 
-// Only minify browser (iife) version
-if (argv.format === "iife") {
-  config.plugins.push(uglify());
-}
+// // Only minify browser (iife) version
+// if (argv.format === "iife") {
+//   config.plugins.push(terser());
+// }
 
 export default config;
 
