1c319f36eeef4268c91966bc4dba3cb450a407e9
Chintan Banugaria <chintan@banugaria.com>
2020-11-05 10:35:10 -0600

    Updated Regex for validating URL
    Added support for domains like
    google.com
    http://gogle.com
    https://google.com

--- src/VueLinkPreview.vue
+++ src/VueLinkPreview.vue
@@ -137,7 +137,7 @@ export default {
   },
   methods: {
     isValidUrl: function(url) {
-      const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+      const regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
       this.validUrl = regex.test(url);
       return this.validUrl;
     },
