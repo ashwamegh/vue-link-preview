2ab4b33e1c326067b1db505038437631e830e6ab
shashank7200 <shashank7200@gmail.com>
2023-12-23 02:55:07 +0530

    added nuxt2 example

---
+++ examples/nuxt2/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <no-ssr placeholder="loading...">
+      <LinkPreview url="https://vuejs.org/" @click="handleClick"> </LinkPreview>
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  components: {
+    // Import the component dynamically
+    [process.client && 'LinkPreview']: () =>
+      import('@ashwamegh/vue-link-preview'),
+  },
+  methods: {
+    handleClick(preview) {
+      console.log(
+        'click',
+        preview.domain,
+        preview.title,
+        preview.description,
+        preview.img
+      )
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
