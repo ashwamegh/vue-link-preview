223f3a51af8bbd8cfde3254c156f3ebc148d8ff1
shashank7200 <shashank7200@gmail.com>
2023-12-23 02:04:08 +0530

    added example for Vue 2

---
+++ examples/vue2/src/App.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/html-self-closing -->
+<script setup>
+import LinkPreview from "@ashwamegh/vue-link-preview";
+</script>
+
+<template>
+    <div id="app">
+        <LinkPreview url="https://vuejs.org/" @click="handleClick">
+        </LinkPreview>
+    </div>
+</template>
+
+<style scoped>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
